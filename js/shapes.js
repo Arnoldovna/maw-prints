5325f650d341d70f5146b5339cc2496cc5fb9509
Arnoldovna <marina.wilhelm@icloud.com>
2020-05-19 19:32:05 +0200

    make shapes responsive, add loops, clean up

--- js/shapes.js
+++ js/shapes.js
@@ -1,8 +1,11 @@
 //matter.js
+
 const {
     Engine,
     Render,
     Bodies,
+    Body,
+    Runner,
     MouseConstraint,
     World,
 } = Matter;
@@ -14,6 +17,7 @@ const w = window.innerWidth;
 const h = window.innerHeight;
 
 const engine = Engine.create();
+const runner = Runner.create();
 
 const render = Render.create({
     element: sectionTag,
@@ -22,6 +26,7 @@ const render = Render.create({
         width: w,
         height: h,
         background: 'white',
+        // wireframes: true,
         wireframes: false,
         pixelratio: window.devicePixelRatio,
     },
@@ -36,46 +41,63 @@ function percentY(percent) {
     return Math.round(percent / 100 * h);
 }
 
-//create Bodies
-const about = Bodies.rectangle(percentX(20), percentY(20), 146, 49, {
-    // frictionAir: 0.1,
-    render: {
-        sprite: {
-            texture: '../../img/about.png',
-        },
-    },
-});
 
-const form1 = Bodies.rectangle(percentX(40), percentY(2), 200, 184, {
-    render: {
-        sprite: {
-            texture: '../../img/form_1.png',
-        },
-    },
-});
+//create Bodies from shapes-data.js
+const bodies = engine.world.bodies;
 
-const form2 = Bodies.rectangle(percentX(30), percentY(5), 324, 100, {
-    render: {
-        sprite: {
-            texture: '../../img/form_2.png',
+shapes.forEach((el, i) => {
+
+    const {
+        name,
+        percW,
+        percH,
+        width,
+        height,
+        radius,
+        sprite,
+        mobile,
+        tablet,
+        desktop
+    } = el;
+    console.log('percentX', name, percentX(percW));
+    World.add(engine.world, Bodies.rectangle(percW, percH, width, height, {
+        chamfer: {
+            radius: radius,
         },
-    },
+        render: {
+            sprite: {
+                texture: "../../img/" + sprite,
+            },
+        },
+    }));
+
+    if (w < 700) {
+        bodies[i].render.sprite.xScale = bodies[i].render.sprite.xScale * mobile;
+        bodies[i].render.sprite.yScale = bodies[i].render.sprite.yScale * mobile;
+        Body.scale(bodies[i], mobile, mobile);
+    } else if (w < 1024) {
+        bodies[i].render.sprite.xScale = bodies[i].render.sprite.xScale * tablet;
+        bodies[i].render.sprite.yScale = bodies[i].render.sprite.yScale * tablet;
+        Body.scale(bodies[i], tablet, tablet);
+
+    } else {
+        return;
+    }
 });
 
 
 //create room
-const walloptions = {
+const wallOptions = {
     isStatic: true,
     // render: {
     //     visible: false,
     // }
 };
 
-const ground = Bodies.rectangle(w / 2, h + 50, w + 100, 100, walloptions);
-const ceiling = Bodies.rectangle(w / 2, -50, w + 100, 100, walloptions);
-
-const leftWall = Bodies.rectangle(w + 50, h / 2, 100, h + 100, walloptions);
-const rightWall = Bodies.rectangle(-50, h / 2, 100, h + 100, walloptions);
+const ground = Bodies.rectangle(w / 2, h + 50, w + 100, 100, wallOptions);
+const ceiling = Bodies.rectangle(w / 2, -50, w + 100, 100, wallOptions);
+const leftWall = Bodies.rectangle(-50, h / 2, 100, h + 10, wallOptions);
+const rightWall = Bodies.rectangle(w + 50, h / 2, 100, h + 10, wallOptions);
 
 //create mouse dynamics
 const mouseControl = MouseConstraint.create(engine, {
@@ -83,25 +105,22 @@ const mouseControl = MouseConstraint.create(engine, {
     constraint: {
         stiffness: 0.8,
         render: {
-            visible: false,
+            visible: true,
         },
     },
 });
-
-// const initialShapes = Composites.stack(50, 50, 15, 5, 40, 40, function (x, y) {
-//   return createShape(x, y);
-// });
 
 World.add(engine.world, [
     ground,
     ceiling,
     leftWall,
     rightWall,
-    about,
-    form1,
-    form2,
     mouseControl,
 ]);
+
+
+
+console.log('bodies', bodies);
 
 //on click add a new shape
 // document.addEventListener('click', function (event) {
@@ -113,18 +132,56 @@ World.add(engine.world, [
 // fit the render viewport to the scene
 
 
+// Runner.run(runner, engine);
 
 Engine.run(engine);
 Render.run(render);
 
 // window.addEventListener("resize", function () {
+// Engine.clear(engine);
+// Render.stop(render);
 
-//     Matter.Body.setPosition(ground, {
-//         x: w / 2,
-//         y: h + 30
-//     });
-//     Matter.Body.setPosition(rightWall, {
-//         x: w + 30,
-//         y: h / 2
-//     });
+// Body.setPosition(ground, {
+//     x: w / 2,
+//     y: h
+// });
+// Body.scale(rightWall, {
+//     scaleY: h * 2
+// });
+// Body.setPosition(rightWall, {
+//     x: w - 200,
+//     y: h
+// });
+// Body.setPosition(leftWall, {
+//     x: 0,
+//     y: h
+// });
+
+// });
+// window.addEventListener('resize', function () {
+//     console.log('heeee resize', engine);
+//     Engine.clear(engine);
+//     console.log('done');
+// })
+
+//Gravity
+let time = 0;
+const changeGravity = function () {
+    time = time + 0.008;
+
+
+    const gravityS = Math.sin(time);
+    const gravity = Math.cos(time);
+    engine.world.gravity.y = gravity;
+    engine.world.gravity.x = gravityS;
+    requestAnimationFrame(changeGravity)
+};
+
+changeGravity();
+window.addEventListener("resize", function () {
+    location.reload();
+});
+// window.addEventListener('deviceOrientation', function (event) {
+//     engine.world.gravity.x = event.beta / 30;
+//     engine.world.gravity.y = event.gamma / 30;
 // });
